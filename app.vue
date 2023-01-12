87672392ecdf3d8920e3fa64f16152a9b7d3fb20
CareTiger <currency@gmail.com>
2023-01-11 21:04:38 -0600

    first commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="grid place-items-center max-w-md mx-auto min-h-screen border border-slate-400">
+    <FormKit label="Default FormKit text input" type="text"
+      help="I recieve Tailwind classes from the FormKit config!" />
+  </div>
+</template>
